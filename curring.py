412df392c1a0257fc700e30c00c62557bcdbe1f3
Vagner Alexander <onemybox@gmail.com>
2023-08-27 20:42:45 +0300

    feat: карринг

---
+++ curring.py
@@ -0,0 +1,27 @@
+from types import FunctionType
+from typing import Union
+
+from pymonad.tools import curry
+
+
+# 2.3.1
+@curry(2)
+def _greeting1(greeting: str, name: str) -> Union[str, FunctionType]:
+    return f"{greeting}, {name}!"
+
+
+hello_name1: FunctionType = _greeting1("Hello")
+
+
+# 2.3.2.
+@curry(4)
+def _greeting2(greeting: str, sep: str, end: str, name: str) -> Union[str, FunctionType]:
+    return f"{greeting}{sep} {name}{end}"
+
+
+hello_name2: FunctionType = _greeting2("Hello")(",")("!")
+
+
+if "__main__" == __name__:
+    print(hello_name1("Tom"))
+    print(hello_name2("Huckleberry"))

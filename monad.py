312695b8b01ca2050e0ac33ee2c6c7e2cac34267
Vagner Alexander <onemybox@gmail.com>
2023-08-31 20:42:43 +0300

    монада

--- monad.py
+++ monad.py
@@ -1,17 +1,17 @@
 from pymonad import Just, Nothing, Maybe
 
 # посадка птиц на левую сторону
-to_left: Maybe = lambda num: lambda balace: (
+to_left: Maybe = lambda num: lambda balance: (
     Nothing
-    if abs((balace[0] + num) - balace[1]) > 4
-    else Just((balace[0] + num, balace[1]))
+    if abs((balance[0] + num) - balance[1]) > 4
+    else Just((balance[0] + num, balance[1]))
 )
 
 # посадка птиц на правую сторону
-to_right: Maybe = lambda num: lambda balace: (
+to_right: Maybe = lambda num: lambda balance: (
     Nothing
-    if abs((balace[1] + num) - balace[0]) > 4
-    else Just((balace[0], balace[1] + num))
+    if abs((balance[1] + num) - balance[0]) > 4
+    else Just((balance[0], balance[1] + num))
 )
 
 # банановая кожура
@@ -30,18 +30,11 @@ begin: Maybe = Just((0, 0))
 
 if '__main__' == __name__:
 
-    show(begin
-         >> to_left(2)
-         >> to_right(5)
-         >> to_left(-2))  # "Упал!" - канатоходец упадёт тут
+    show(begin >> to_left(2) >> to_right(5) >> to_left(-2))
+    # "Упал!" - канатоходец упадёт тут
 
-    show(begin
-         >> to_left(2)
-         >> to_right(5)
-         >> to_left(-1))  # (1, 5) - в данном случае всё ок
+    show(begin >> to_left(2) >> to_right(5) >> to_left(-1))
+    # (1, 5) - в данном случае всё ок
 
-    show(begin
-         >> to_left(2)
-         >> banana  # "Упал!" кожура всё испортит
-         >> to_right(5)
-         >> to_left(-1))
+    show(begin >> to_left(2) >> banana >> to_right(5) >> to_left(-1))
+    # "Упал!" кожура всё испортит

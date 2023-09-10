d233375e26ca5b487ac18bc8a0478262bfdd4f14
Vagner Alexander <onemybox@gmail.com>
2023-09-10 13:26:00 +0300

    Поездка на мотоцикле

---
+++ odometer.py
@@ -0,0 +1,24 @@
+from functools import reduce
+from typing import Callable, List
+
+
+# функция расчета дельты времени
+delta_time: Callable[[List[int]], List[int]] = lambda time: [time[0]] + list(
+    map(lambda x, y: x - y, time[1:], time[:-1])
+)
+
+# функция получения массивов времени и скорости
+get_time_and_speed: Callable[[List[int]], List[int]] = lambda massive: (
+    delta_time(massive[1::2]), massive[0::2]
+)
+
+# итоговая функция расчета пути
+odometr: Callable[[List[int]], int] = lambda massive: reduce(
+    lambda x, y: x + y,
+    map(lambda x, y: x * y, *get_time_and_speed(massive))
+)
+
+
+if "__main__" == __name__:
+    print(odometr([15, 1, 25, 2, 30, 3, 10, 5]))  # 90
+    print(odometr([10, 1, 20, 2]))  # 30
